faed7c24cec3d4c94ae06a1cfa6cdc0a67c355e7
Timothy Kipkorir Serem <theecodepoet@gmail.com>
2020-07-10 15:27:37 +0300

    Increment and decrement

---
+++ Problems/Decrement all/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // put your code here
+        int number1 = scanner.nextInt() - 1;
+        int number2 = scanner.nextInt() - 1;
+        int number3 = scanner.nextInt() - 1;
+        int number4 = scanner.nextInt() - 1;
+        System.out.println(number1 + " " + number2 + " " + number3 + " " + number4);
+    }
+}
