7dc68887b3594a36b1ee9c171af2b457cd398450
Timothy Kipkorir Serem <theecodepoet@gmail.com>
2020-07-13 19:03:58 +0300

    Reading integer numbers

---
+++ Problems/Reading integer numbers/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        int num3 = scanner.nextInt();
+        int num4 = scanner.nextInt();
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+    }
+}
