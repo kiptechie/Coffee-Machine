0a8e739d61c38d3ae11381124aea5891e40bef59
Timothy Kipkorir Serem <theecodepoet@gmail.com>
2020-07-14 16:34:19 +0300

    Relational operators -> Check the given numbers are different

---
+++ Problems/Check the given numbers are different/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // put your code here
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        System.out.println((a == b) && (a == c));
+    }
+}
